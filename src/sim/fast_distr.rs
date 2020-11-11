960a9d9f2bf73d3cd87aee7265f4ee1c0eb885a5
zachmatson <zacharywdmatson@gmail.com>
2020-11-11 11:17:35 -0500

    Fix small Poisson issue

--- src/sim/fast_distr.rs
+++ src/sim/fast_distr.rs
@@ -6,9 +6,7 @@ use super::*;
 /// Sample a Poisson random variate from a distribution with mean `lambda` using provided `rng` 
 /// Will *panic* on invalid lambda 
 pub fn poisson<R: Rng>(lambda: f64, rng: &mut R) -> u64 {
-    if lambda <= 0.0 {
-        panic!("NEGATIVE LAMBDA NOT ALLOWED")
-    } else if lambda <= 10.0 {
+    if lambda <= 10.0 {
         direct_poisson(lambda, rng)
     } else {
         rand_distr::Poisson::new(lambda).unwrap().sample(rng)
