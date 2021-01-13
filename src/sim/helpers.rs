7479f6fb176f75ce9b94ec9aa6fc42376457e28d
zachmatson <zacharywdmatson@gmail.com>
2021-01-13 16:29:30 -0500

    Enable actual multiple beneficial/neutral mutants

--- src/sim/helpers.rs
+++ src/sim/helpers.rs
@@ -158,7 +158,7 @@ fn add_mutants<R: Rng>(
                     }
                 }
 
-                let mutant = new_mutant(lineage, cfg, rng);
+                let mutant = new_mutant(lineage, mutant_order, cfg, rng);
                 data.push_child(mutant, lineage, mutations_vec);
                 // N still includes the mutants that come from the lineage up until this point
                 // No need to update `lineage` because its N field is not used here
@@ -174,19 +174,23 @@ fn add_mutants<R: Rng>(
 }
 
 /// Generate a descendant lineage from `parent`
-fn new_mutant<R: Rng>(parent: Lineage, cfg: &SimConfig, rng: &mut R) -> Lineage {
-    let mutation_type = cfg.sample_mutation_type(rng).unwrap();
+fn new_mutant<R: Rng>(parent: Lineage, order: u32, cfg: &SimConfig, rng: &mut R) -> Lineage {
+    let mut W = parent.W;
+    let mut lambda = parent.secondary.lambda;
 
-    let (W, lambda) = match mutation_type {
-        MutationType::Beneficial => updates_after_beneficial_mutation(parent, cfg, rng),
-        MutationType::Deleterious => updates_after_deleterious_mutation(parent, cfg, rng),
-        MutationType::Neutral | MutationType::MutationRate => (parent.W, parent.secondary.lambda),
-    };
+    for _ in 0..order {
+        let mutation_type = cfg.sample_mutation_type(rng).unwrap();
 
-    // let U = match mutation_type {
-    //     MutationType::MutationRate => mutation_rate_todo(),
-    //     _ => parent.U,
-    // };
+        match mutation_type {
+            MutationType::Beneficial => {
+                let size = beneficial_mutation_size(parent, rng);
+                let results = apply_beneficial_mutation((W, lambda), size, cfg);
+                W = results.0;
+                lambda = results.1;
+            }
+            _ => (),
+        }
+    }
 
     Lineage {
         N: 1.0,
@@ -200,27 +204,29 @@ fn new_mutant<R: Rng>(parent: Lineage, cfg: &SimConfig, rng: &mut R) -> Lineage 
 }
 
 /// Generate fitness and mutation size lambda of a descendant of `parent` after undergoing a beneficial mutation
-fn updates_after_beneficial_mutation<R: Rng>(
-    parent: Lineage,
-    cfg: &SimConfig,
-    rng: &mut R,
-) -> (f64, f64) {
-    let mutation_size = rand_distr::Exp::new(parent.secondary.lambda)
+fn beneficial_mutation_size<R: Rng>(parent: Lineage, rng: &mut R) -> f64 {
+    rand_distr::Exp::new(parent.secondary.lambda)
         .unwrap()
-        .sample(rng);
-    let lambda_new = parent.secondary.lambda
-        * (1.0 + cfg.diminishing_returns_epistasis_strength * mutation_size);
-    let W_new = parent.W * (1.0 + mutation_size);
-
-    (W_new, lambda_new)
+        .sample(rng)
 }
 
 /// Generate fitness and mutation size lambda of a descendant of `parent` after undergoing a deleterious mutation
-#[allow(unused_variables)]
-fn updates_after_deleterious_mutation<R: Rng>(
-    parent: Lineage,
-    cfg: &SimConfig,
-    rng: &mut R,
-) -> (f64, f64) {
+#[allow(unused_variables, dead_code)]
+fn deleterious_mutation_size<R: Rng>(parent: Lineage, rng: &mut R) -> f64 {
     deleterious_todo()
+}
+
+/// Apply a beneficial mutation to an existing fitness  
+/// Takes `W` and `lambda` for existing lineage as well as `mutation_size` and returns
+/// `(W, lambda)` tuple after the mutation
+#[inline(always)]
+fn apply_beneficial_mutation(
+    (W, lambda): (f64, f64),
+    mutation_size: f64,
+    cfg: &SimConfig,
+) -> (f64, f64) {
+    (
+        W * (1.0 + mutation_size),
+        lambda * (1.0 + cfg.diminishing_returns_epistasis_strength * mutation_size),
+    )
 }
