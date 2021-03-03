59a6d4141b0fe81d3c4859dae1e43f0b7c0d429a
zachmatson <zacharywdmatson@gmail.com>
2021-03-03 11:12:55 -0500

    Switch to SLEEF and explicit AVX2 SIMD from C kernel side. Eliminates SIMD FFI calls from previous SLEEF/explicit SIMD version

--- build.rs
+++ build.rs
@@ -1,17 +1,10 @@
-use std::process::Command;
-use std::env;
+use std::{process::{exit, Command}, env, error::Error};
 
-fn main() {
-    // Rerun if the kernels or Makefile are changed
-    println!("cargo:rerun-if-changed=src/sim/C_kernels/Makefile");
-    println!("cargo:rerun-if-changed=src/sim/C_kernels/kernels.c");
-
-    let out_dir = env::var("OUT_DIR").unwrap();
+fn build_kernels_inner() -> Result<bool, Box<dyn Error>> {
     let target_features = env::var("CARGO_CFG_TARGET_FEATURE").unwrap();
     let avx2_available = target_features.contains("avx2");
 
-    // Build the kernels with make
-    Command::new("make")
+    Ok(Command::new("make")
         // Run from the kernels directory
         .current_dir("src/sim/C_kernels/")
         // -B forces make to rebuild, since cargo has already decided a rebuild is needed,
@@ -19,10 +12,37 @@ fn main() {
         .args(&["-B", "kernels"])
         // Tell make whether to use AVX2
         .env("KERNELS_USE_AVX2", format!("{}", avx2_available as u8))
-        .spawn()
-        .expect("Failed to build C kernels");
+        // Provide SLEEF directory
+        .env("SLEEF_HEADER_DIR", sleef_shim::get_header_dir())
+        .spawn()?
+        .wait()?
+        .success())
+}
+
+fn build_kernels() -> String {
+    match build_kernels_inner() {
+        Err(_) | Ok(false) => {
+            eprintln!("Failed to build RELLTEE C kernels");
+            exit(1);
+        }
+        _ => ()
+    }
+
+    env::var("OUT_DIR").unwrap()
+}
+
+fn main() {
+    // Rerun if the kernels or Makefile are changed
+    println!("cargo:rerun-if-changed=src/sim/C_kernels/Makefile");
+    println!("cargo:rerun-if-changed=src/sim/C_kernels/kernels.c");
+
+    // Build the kernels with make
+    let relltee_lib_dir = build_kernels();
     
     // Link the kernels
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-search=native={}", relltee_lib_dir);
     println!("cargo:rustc-link-lib=static=rellteekernels");
+    // Link SLEEF
+    println!("cargo:rustc-link-search=native={}", sleef_shim::get_lib_dir().display());
+    println!("cargo:rustc-link-lib=static=sleef");
 }
--- src/sim/C_kernels/kernels.c
+++ src/sim/C_kernels/kernels.c
@@ -1,19 +1,35 @@
 #include <stddef.h>
 #include <math.h>
+#ifdef KERNELS_USE_AVX2
+    #include <immintrin.h>
+    #include "sleef.h"
+#endif
 
-void grow_lineages_inplace_c(size_t len, double N[len], double W[len], double delta_t) {
+void grow_lineages_inplace_c(size_t len, double *N, const double *W, const double delta_t) {
     #ifdef KERNELS_USE_AVX2
-        const double log2 = log(2);
-        #pragma omp simd
-        for (size_t i = 0; i < len; ++i) {
-            N[i] *= exp(log2 * W[i] * delta_t);
-            N[i] = ceil(N[i]);
+        const size_t VEC_LEN = 4;
+        const size_t main_len = len / VEC_LEN;
+        const size_t remainder_len = len % VEC_LEN;
+        const __m256d delta_t_packed = _mm256_set1_pd(delta_t);
+
+        for (size_t i = 0; i < main_len; ++i) {
+            __m256d N_vec = _mm256_loadu_pd(N);
+            const __m256d W_vec = _mm256_loadu_pd(W);
+
+            __m256d temp = _mm256_mul_pd(W_vec, delta_t_packed);
+           temp = Sleef_exp2d4_u10avx2(temp);
+           temp = _mm256_mul_pd(N_vec, temp);
+           temp = _mm256_ceil_pd(temp);
+           _mm256_storeu_pd(N, temp);
+
+           N += VEC_LEN;
+           W += VEC_LEN;
         }
-    #else
-        #pragma omp simd
-        for (size_t i = 0; i < len; ++i) {
-            N[i] *= exp2(W[i] * delta_t);
-            N[i] = ceil(N[i]);
-        } 
+
+        len = remainder_len;
     #endif
+    for (size_t i = 0; i < len; ++i) {
+        N[i] *= exp2(W[i] * delta_t);
+        N[i] = ceil(N[i]);
+    }
 }
