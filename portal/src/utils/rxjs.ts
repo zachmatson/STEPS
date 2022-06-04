0c93a60ab2c0c7356b81e16935c39b954349c202
zachmatson <zacharywdmatson@gmail.com>
2022-06-04 16:27:38 -0400

    Another good state

--- portal/src/utils/rxjs.ts
+++ portal/src/utils/rxjs.ts
@@ -1,11 +1,9 @@
-import { map, Observable, Subject } from "rxjs";
-
-export const discardValues = () => map(() => {});
+import { Observable, Subject } from "rxjs";
 
 export const toSubscribedSubject =
   <T>(subjectFactory: () => Subject<T> = () => new Subject<T>()) =>
-  (source$: Observable<T>): Observable<T> => {
+  (source: Observable<T>): Observable<T> => {
     const subject = subjectFactory();
-    source$.subscribe(subject);
-    return subject.asObservable();
+    source.subscribe(subject);
+    return subject;
   };
--- portal/src/utils/typescript.ts
+++
@@ -1,5 +0,0 @@
-export type FilterUnionByType<UnionInstance, Type> = UnionInstance extends {
-  type: Type;
-}
-  ? UnionInstance
-  : never;
