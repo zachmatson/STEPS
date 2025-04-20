43576b92e6474d927d20ea36f01c5bf371d8167c
zachmatson <zacharywdmatson@gmail.com>
2025-04-20 19:37:13 -0400

    Refactor the processing of SimLink events a bit

--- portal/src/simulations/SimLink.ts
+++ portal/src/simulations/SimLink.ts
@@ -1,25 +1,24 @@
 import { SimOutcome, SimResultsFragments } from "./simTypes";
 import { SimWorkerHandle, SimWorkerOutboundMessage } from "./workerInterface";
 import { PortalRunConfig } from "../config/PortalRunConfig";
-import { Observable, Subject } from "rxjs";
+import { Subject } from "rxjs";
 
-export interface SimObservables {
-  start$: Observable<PortalRunConfig>;
-  results$: Observable<SimResultsFragments>;
-  outcome$: Observable<SimOutcome>;
-}
+export type SimEvent =
+  | { type: "started"; config: PortalRunConfig }
+  | { type: "results"; results: SimResultsFragments }
+  | { type: "done"; outcome: SimOutcome };
 
+/**
+ * SimLink is responsible for serving as an interface between the simulations and the rest of the portal code,
+ * abstracting away the details of the worker and WASM and exposing a stream of events.
+ */
 export class SimLink {
-  #start$: Subject<PortalRunConfig> = new Subject();
-  #results$: Subject<SimResultsFragments> = new Subject();
-  #outcome$: Subject<SimOutcome> = new Subject();
-  #publicObservables: SimObservables = {
-    start$: this.#start$.asObservable(),
-    results$: this.#results$.asObservable(),
-    outcome$: this.#outcome$.asObservable(),
-  };
-
+  readonly #event$ = new Subject<SimEvent>();
   #workerHandle: SimWorkerHandle | undefined;
+
+  eventsObservable() {
+    return this.#event$.asObservable();
+  }
 
   startOrRestart(config: PortalRunConfig) {
     // Stop the old worker, no messages from the old worker will be processed any more
@@ -34,19 +33,22 @@ export class SimLink {
     // Inform listeners that a new simulation run has started
     // This is guranteed to run before we process any messages from the new worker, because we haven't yielded to the
     // event loop yet.
-    this.#start$.next(config);
+    this.#event$.next({ type: "started", config });
   }
 
   #onWorkerMessage(data: SimWorkerOutboundMessage, config: PortalRunConfig) {
     switch (data.type) {
       case "ready":
+        // The worker is ready to start, so we will tell it to
         this.#workerHandle?.postMessage({ type: "start", config: config });
         break;
       case "results":
-        this.#results$.next(data.results);
+        // Forward results to listeners
+        this.#event$.next({ type: "results", results: data.results });
         break;
       case "done":
-        this.#outcome$.next(data.outcome);
+        // Forward outcome to listeners
+        this.#event$.next({ type: "done", outcome: data.outcome });
         break;
     }
   }
@@ -57,9 +59,5 @@ export class SimLink {
 
   resume() {
     this.#workerHandle?.postMessage({ type: "resume" });
-  }
-
-  observables(): SimObservables {
-    return this.#publicObservables;
   }
 }
--- portal/src/utils/rxjsUtils.ts
+++ portal/src/utils/rxjsUtils.ts
@@ -1,9 +1,81 @@
 import { Observable, Subject } from "rxjs";
 
-export const toSubscribedSubject =
-  <T>(subjectFactory: () => Subject<T> = () => new Subject<T>()) =>
-  (source: Observable<T>): Observable<T> => {
-    const subject = subjectFactory();
-    source.subscribe(subject);
-    return subject;
-  };
+/**
+ * Buffer values from the source observable until the returned observable gets a subscriber.
+ *
+ * The first subscriber will get all of the values that have been buffered; any subsequent subscribers are not
+ * guaranteed to see values from before they have subscribed.
+ *
+ * Regardless of unsubscriptions, buffering will stop after the first subscriber subscribes and the buffer has been
+ * drained.
+ */
+export function bufferUntilSubscribed<T>(source: Observable<T>): Observable<T> {
+  // Underlying buffer which provides appropriate atomicity guarantees
+  const buffer = new ObservableBuffer(source);
+  // Subject which we will use to send values to observers
+  const subject = new Subject<T>();
+  // Track whether we have gotten any subscribers
+  let hasBeenSubscribedTo = false;
+
+  return new Observable<T>((observer) => {
+    // Immediately subscribe to the subject
+    // If this is the first subscriber, nothing will happen immediately, and we will begin to push to the subject below
+    // For subsequent subscribers, this will either immediately start picking up values from the source, or it may
+    // pick up some values being sent as the buffer drains.
+    const subscription = subject.subscribe(observer);
+
+    // Send values to first subscriber
+    if (!hasBeenSubscribedTo) {
+      hasBeenSubscribedTo = true;
+
+      // Send all buffered values to the new subscriber
+      // Atomically take chunks of values from the buffer and send them, in case observer.next has some side effect
+      // which causes more values to be sent on the source, or yields to the event loop
+      let bufferedValues;
+      do {
+        bufferedValues = buffer.takeAccumulatedValues();
+        bufferedValues.forEach((value) => subject.next(value));
+      } while (bufferedValues.length > 0);
+      // Finally, once there are no buffered values left, we can atomically transfer the subscription to our subject
+      buffer.bypassBuffer((value) => subject.next(value));
+    }
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
+}
+
+/**
+ * Buffer an observable and use a "pull" model to get out buffered chunks
+ */
+class ObservableBuffer<T> {
+  #buffer: T[] = [];
+  #sourceObserver: (next: T) => void = () => {};
+
+  constructor(source: Observable<T>) {
+    // Set the buffer to "buffering" mode
+    this.startBuffering();
+    // Pass a closure capturing `this`. Then we can change #sourceObserver out without interacting with `source`.
+    source.subscribe((next) => this.#sourceObserver(next));
+  }
+
+  startBuffering() {
+    this.#sourceObserver = (next) => this.#buffer.push(next);
+  }
+
+  bypassBuffer(observer: (next: T) => void) {
+    this.#sourceObserver = observer;
+  }
+
+  /**
+   * Atomically take whichever values have accumulated since the last time this method was called
+   */
+  takeAccumulatedValues(): T[] {
+    // Atomically replace buffer with empty list
+    const existingValues = this.#buffer;
+    this.#buffer = [];
+    // Return buffered values
+    return existingValues;
+  }
+}
