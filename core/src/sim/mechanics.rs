72e10fb78b827c141c8e6c8b2fbedcea3d982b9b
Devin Lake <devinmlake@yahoo.com>
2025-06-24 13:35:16 -0400

    Epistasis Error Fixed

--- core/src/sim/mechanics.rs
+++ core/src/sim/mechanics.rs
@@ -269,8 +269,8 @@ fn apply_deleterious_mutation<R: Rng>(lineage: &mut Lineage, cfg: &InternalSimCo
     };
 
     lineage.W *= 1.0 - size;
-    let G = cfg.inner.diminishing_returns_epistasis_strength / (size * (1.0 - cfg.inner.diminishing_returns_epistasis_strength) + 1.0);
-    lineage.secondary.lambda *= 1.0 + G * size;
+    let G = cfg.inner.diminishing_returns_epistasis_strength / (size * (cfg.inner.diminishing_returns_epistasis_strength - 1.0) + 1.0);
+    lineage.secondary.lambda *= 1.0 - G * size;
 }
 
 /// Get next float for finite floats
